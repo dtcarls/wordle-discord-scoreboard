13f06d84ef716b6e96238664036a04bc746cd64f
dean.carlson <dean.t.carlson1@navy.mil>
2022-03-12 08:27:48 -0500

    fixed typo

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
-kill -9 $(ps aux | grep -e "wordle_boy.py" | awk '{ print $2 }')
+kill -9 $(ps aux | grep -e "wordle_bot.py" | awk '{ print $2 }')
 kill -9 $(ps aux | grep -e "missed_games.py" | awk '{ print $2 }')
